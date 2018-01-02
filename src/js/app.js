7d1cc259e489019e6f3868c7ff6f8479c8981306
alkal <alex.kalamatianos@eurodyn.com>
2018-01-02 18:08:20 +0200

    Webpack setup for pure HTML-CSS-SASS-JS project

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,21 @@ import 'jquery';
 import '../css/main.scss';
 import '../css/custom.css';
 import './console-message';
+import {RandomGenerator} from './random-generator';
+
+const outputParagraph = $('#outputParagraph');
+
+const outputRandomInt = () => {
+    outputParagraph.text(RandomGenerator.randomInteger());
+};
+
+const outputRandomRange = () => {
+    outputParagraph.text(RandomGenerator.randomRange(1, 500));
+};
+
+const buttonRndInt = jQuery('#randomInt');
+const buttonRndRange = $('#randomRange');
+
+buttonRndInt.click(outputRandomInt);
+buttonRndRange.click(outputRandomRange);
+>>>>>>> Webpack setup for pure HTML-CSS-SASS-JS project
